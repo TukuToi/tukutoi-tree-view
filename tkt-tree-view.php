56fc7870079397cbbb521e47115e79e0c70da86d
Beda Schmid <arbeit@yoonect.com>
2020-03-22 23:20:53 +0700

    Update...
     to installer and proper, toolset free rendering

---
+++ tkt-tree-view.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Plugin Name: Hierarchical Posts Tree
+ * Description: WordPress Dashboard Widget displaying a Tree View of Hierarchical Pages
+ * Plugin URI: https://github.com/TukuToi/ToolWine-TreeView
+ * Author: Beda Schmid
+ * Author URI: https://tukutoi.com
+ * Version: 1.0.0
+ * License: GPL2
+ * Text Domain: tkt-tree-view
+ */
+
+//Do not access directly
+if (!defined('ABSPATH')) exit;
+
+if (!defined('PLUGIN_DIR_PATH')) {
+	define('PLUGIN_DIR_PATH', plugin_dir_url( __FILE__ ));
+}
+
+include_once(plugin_dir_path(__FILE__).'Application/tkt-functions.php');
+add_action( 'admin_enqueue_scripts', 'tkt_enqueue_styles_and_scripts' );
+
+include_once(plugin_dir_path(__FILE__).'widget.php');
+add_action( 'wp_dashboard_setup', 'tkt_treeview_dashboard_widget' );
+
+include_once(plugin_dir_path(__FILE__).'update.php');
